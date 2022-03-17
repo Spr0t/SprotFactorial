4790506d72482630cdb47997337e33986c945a01
Dell <Dell@DESKTOP-B6FMJUN>
2022-03-17 14:51:13 +0200

    Добавьте файлы проекта.

---
+++ LessonHW8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LessonHW8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число для вычисления факториала: ");
+            int n = int.Parse(Console.ReadLine());
+            long factorial = 1;
+
+            for (int j = 1; j <= n; j++)
+            {
+                factorial *= j;
+            }
+
+            Console.WriteLine($"Факториал равен: {factorial}");
+            Console.ReadLine();
+
+        }
+    }
+}
